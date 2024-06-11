3f14779f1d3207dafd21fbfb2027c85072f63b36
salma el bakkouri <elbakkourisalmaa@gmail.com>
2024-06-11 23:53:22 +0100

    changed structure of dashboard page

--- src/components/Dashboardpage.js
+++ src/components/Dashboardpage.js
@@ -1,14 +1,52 @@
-import React, { useEffect, useState, useRef } from 'react';
-
+import React from 'react';
+// import '../css/dashboardpage.css';
+import { FaUser, FaHeartbeat, FaProcedures, FaStethoscope } from 'react-icons/fa';
+import doctorImage from '../assets/doctor.png';
 
 const Dashboardpage = () => {
     return (
-        <div className="">
-          helllo worl dashboard
-
+        <div className="dashboard-container">
+            <div className="stats-container">
+                <div className="stat-box">
+                    <div className="stat-icon"><FaUser /></div>
+                    <div className="stat-info">
+                        <div className="stat-number">1200</div>
+                        <div className="stat-label">patients</div>
+                    </div>
+                </div>
+                <div className="stat-box">
+                    <div className="stat-icon"><FaHeartbeat /></div>
+                    <div className="stat-info">
+                        <div className="stat-number">2000</div>
+                        <div className="stat-label">séances</div>
+                    </div>
+                </div>
+                <div className="stat-box">
+                    <div className="stat-icon"><FaProcedures /></div>
+                    <div className="stat-info">
+                        <div className="stat-number">702</div>
+                        <div className="stat-label">patients de Kinésithérapie</div>
+                    </div>
+                </div>
+                <div className="stat-box">
+                    <div className="stat-icon"><FaStethoscope /></div>
+                    <div className="stat-info">
+                        <div className="stat-number">524</div>
+                        <div className="stat-label">patients de Physiothérapie</div>
+                    </div>
+                </div>
+            </div>
+            <div className="header-container">
+                <div className="header-info">
+                    <div className="header-title">Tableau de Bord</div>
+                    <div className="header-subtitle">Centre Arbouni</div>
+                </div>
+                <div className="header-image">
+                    <img src={doctorImage} alt="Doctor" />
+                </div>
+            </div>
         </div>
-      );
-    };
-    
-    export default Dashboardpage;
-    
+    );
+};
+
+export default Dashboardpage;
